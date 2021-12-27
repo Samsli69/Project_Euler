b01b12d2c22d3096d77248f712d52be677dc0d53
Sam <Samir.sli69@outlook.fr>
2021-12-27 17:04:33 +0100

    Exo 2

---
+++ Exo 2/Program.cs
@@ -0,0 +1,19 @@
+﻿int somme = 0;
+int temp;
+int a = 0;
+int b = 1;
+while (somme < 4000000)
+{
+    temp = b;
+    b = a + b;
+    a = temp;
+    if (b % 2 == 0)
+    {
+        somme += b;
+        Console.WriteLine(somme);
+    }
+}
+
+
+
+
